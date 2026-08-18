47f2e24973dbbfc512f7f675e52f0700852a9a14
Brenosilvxz <josephbreno72@gmail.com>
2026-08-18 16:00:09 -0300

    feat: add components

--- open-pizzaria/src/App.jsx
+++ open-pizzaria/src/App.jsx
@@ -1,43 +1,18 @@
+import Header from "./Components/Header.jsx";
+import Banner from "./Components/Banner.jsx";
+import Cards from "./Components/Cards.jsx";
+import './Global.css'
 function App() {
   return (
-    <>
-      <header>
-        <h1>🍕 Bem-vindo à OpenPizzaria!</h1>
-        <p>
-          As melhores pizzas, preparadas com carinho para deixar seu momento
-          ainda mais saboroso.
-        </p>
-      </header>
-      <hr />
+    
+    <div className="container">
+      <Header />
 
-      <section className="banner">
-        <img
-            src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?auto=format&fit=crop&w=1000&h=300"
-    alt="Pizza deliciosa da OpenPizzaria"
-          width="1000"
-          height="300"
-        />
-      </section>
-      <section className="Sobre-pizzaria">
-        <article>
-          <h1>🍕 Pizzas Artesanais</h1>
-          <p>
-            Na OpenPizzaria, preparamos nossas pizzas com ingredientes frescos,
-            massa saborosa e muito carinho. Escolha seus sabores favoritos e
-            aproveite uma pizza deliciosa!
-          </p>
-        </article>
+      <Banner />
 
-        <article>
-          <h1>🔥 Sabor que Chega Quentinho</h1>
-          <p>
-            Do forno direto para a sua mesa! Nosso objetivo é entregar uma pizza
-            quentinha, crocante e cheia de sabor para você aproveitar cada
-            pedaço.
-          </p>
-        </article>
-      </section>
-    </>
+      <Cards />
+      </div>
+  
   );
 }
 
---
+++ open-pizzaria/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <>
+    <header>
+      <h1>🍕 Bem-vindo à OpenPizzaria!</h1>
+      <p>
+        As melhores pizzas, preparadas com carinho para deixar seu momento ainda
+        mais saboroso.
+      </p>
+    </header>
+    </>
+  );
+};
+
+export default Header;
