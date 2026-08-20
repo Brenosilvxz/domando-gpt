20456df28e1b736b4d592c9105a5c1c73698fc17
Brenosilvxz <josephbreno72@gmail.com>
2026-08-20 16:25:30 -0300

    feat: adicionando novos componentes

--- open-pizzaria/src/App.jsx
+++ open-pizzaria/src/App.jsx
@@ -1,18 +1,27 @@
 import Header from "./Components/Header.jsx";
 import Banner from "./Components/Banner.jsx";
 import Cards from "./Components/Cards.jsx";
-import './Global.css'
+import Footer from "./Components/Footer.jsx";
+import Menu from "./Components/Menu.jsx";
+
+import "./Global.css";
 function App() {
   return (
-    
     <div className="container">
-      <Header />
+      <Header
+        tittleHeader="Bem-vindo à OpenPizzaria!"
+        textHeader=" As melhores pizzas, preparadas com carinho para deixar seu momento ainda
+        mais saboroso."
+      />
+
+      <Menu />
 
       <Banner />
 
       <Cards />
-      </div>
-  
+
+      <Footer />
+    </div>
   );
 }
 
--- open-pizzaria/src/Components/Header.jsx
+++ open-pizzaria/src/Components/Header.jsx
@@ -1,16 +1,14 @@
-import './Header.css'
-const Header = () => {
+import "./Header.css";
+const Header = (props) => {
+  console.log(props);
   return (
     <>
-    <header className="header"> 
-         <div className="header-content">
-      <h1>🍕 Bem-vindo à OpenPizzaria!</h1>
-      <p>
-        As melhores pizzas, preparadas com carinho para deixar seu momento ainda
-        mais saboroso.
-      </p>
-      </div>
-    </header>
+      <header className="header">
+        <div className="header-content">
+          <h1>{props.tittleHeader}</h1>
+          <p>{props.textHeader}</p>
+        </div>
+      </header>
     </>
   );
 };
